572ff864938789ae2bf3da8c2a92c5764ca5d6c4
sinatrya <222011528@stis.ac.id>
2023-09-27 13:39:05 +0700

    auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+service('auth')->routes($routes, ['except' => ['login', 'register']]);
+$routes->get('login', 'Auth\LoginController::loginView');
+$routes->get('register', 'Auth\RegisterController::registerView');
+$routes->post('register', 'Auth\RegisterController::registerAction');
