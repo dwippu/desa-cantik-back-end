42ebeaab4ba13055b0c6d091823c7284257593bc
Lasta Sabila Krisyolin <222011678@stis.ac.id>
2023-09-30 10:54:51 +0700

    tampilkan profil desa

--- app/Controllers/ProfileDesa.php
+++ app/Controllers/ProfileDesa.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\WilayahModel;
+use App\Models\WilayahUserModel;
+use App\Models\ProfilDesaModel;
 
 class ProfileDesa extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('profile_desa');
+        $id = auth()->getUser()->id;
+        $user = new WilayahUserModel();
+        $wilayah = new WilayahModel();
+        $info_desa = $wilayah->find($user->getWilayah($id));
+
+        $profil = new ProfilDesaModel();
+        $profil_desa = $profil->nowProfil($info_desa['kode_desa']);
+
+        $data = ['info_desa' => $info_desa, 'profil_desa'=>$profil_desa];
+        return view('profile_desa', $data);
+    }
+
+    public function pengajuan()
+    {
+        
     }
 }
---
+++ app/Models/WilayahUserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Shield\Models\GroupModel;
+
+class WilayahUserModel extends GroupModel
+{
+    /**
+     * @param String|int $id dari user yang akan diubah wilayahnya
+     * @param String $wilayah kode desa yang yang baru
+     */
+    public function setWilayah($id, String $wilayah)
+    {
+        $usergroup = $this->where('user_id', $id)->findAll();
+        $this->update($usergroup[0]['id'], ['kode_desa' => $wilayah]);
+    }
+    
+    public function getWilayah($id){
+        $usergroup = $this->where('user_id', $id)->findAll();
+        return $usergroup[0]['kode_desa'];
+    }
+}
